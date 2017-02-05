d515428ef27b57d5f389713324d10c784aab6f4c
shijunfeng <johnshiluck@gmail.comgit>
2017-02-05 22:09:20 +0800

    add bootstrap flash info

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,5 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:info]="早安！你好！"
+  end
 end
--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,11 +1,11 @@
 module FlashesHelper
-  FLASH_CLASSES ={ alert: 'danger', notic: 'success', warning: 'warning'}.freeze
+  FLASH_CLASSES ={ alert: 'danger', notic: 'success', warning: 'warning',info:'info'}.freeze
   def flash_class(key)
     FLASH_CLASSES.fetch key.to_sym, key
   end
 
   def user_facing_flashes
-    flash.to_hash.slice "alert", "notice", "warning"
+    flash.to_hash.slice "alert", "notice", "warning","info"
   end
 
 end
