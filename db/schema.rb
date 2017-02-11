5f09cc03230a9f524de8110e507f2668e094832a
shijunfeng <johnshiluck@gmail.comgit>
2017-02-11 08:22:33 +0800

    people can't see edit button unless he is group owner

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170210160215) do
+ActiveRecord::Schema.define(version: 20170210235518) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.text     "description"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
